8e54935bd3e86b27f2e34569fbeb628b2767503d
Bruce Quach <quachbruce@gmail.com>
2022-10-02 11:30:34 -0500

    Change default routing path

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ return (
     <Router>
     <Navbar />
     <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route exact path='/website' element={<Home />} />
         <Route exact path='/about' element={<About/>} />                
         <Route exact path='/qualifications' element={<Qualifications/>} />
         <Route exact path='/links' element={<Links/>} />

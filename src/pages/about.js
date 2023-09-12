f8e638f5f7ce4c90e79d6c9ef8f5656bdf8f75f7
Bruce Quach <quachbruce@gmail.com>
2023-09-11 19:04:28 -0500

    Implement timeline for professional experience

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,76 +1,73 @@
 import React from "react";
-import '../css/about.css';
-  
+import "../css/about.css";
+
 const About = () => {
   return (
     <div class="wrapper">
-      <img style={styles.profilePicture} src={require('./images/personal_picture.jpg')} alt="Headshot"/>
-      <h1 class="header">
-        Greetings!
-      </h1>
-        <p>
-          Hi, I'm Bruce. I'm a junior computer science student at 
-          Louisiana State University.
-        </p>
-      <h1 class="header">
-        Who I Am
-      </h1>
-        <p>
-          Aside from completing two years at LSU studying software engineering,
-          I'm also a Software Development Junior Associate at Rural Sourcing, Inc.
-          in the Baton Rouge development center, a web development intern at
-          SciQuel, a science media project and startup, and a senior student
-          worker at LSU IT Services. I'm always looking to expand my project
-          horizons and learn new skills.
-        </p>  
-      <h1 class="header">
-        My Qualifications and Professional Interests
-      </h1>
-        <p>
-          I have a good deal of front-end development and some back-end
-          development experiences from classwork and student worker experiences.
-          I've also completed a summer internship in 2022 at DXC Technology, 
-          a software consulting company in New Orleans, LA, working with AWS and
-          MySQL databases to perform data ETL services, as well as a summer internship at
-          Runatek in 2022, a medtech startup in Dallas, Texas, working with React Native, Arduino
-          code, and materials to work towards creating useful and beneficial medical devices.
-          I'm working towards acquiring a software engineering degree, so I'm 
-          primarily interested in that field. I really like working with UI/UX development 
-          and learning more about web API and framework development, though. Please see 
-          "Contact" info if interested in working with me, as I'm always willing 
-          to work with new people and learn more about software, web development,
-          and other subjects!
-        </p>
-      <h1 class="header">
-        Some Fun Facts
-      </h1>
-        <p>
-          I really like listening to K-Pop and watching anime and K-Dramas.
-          My favorite anime right now are probably Bleach, Black Clover,
-          and Bungo Stray Dogs. I'm always willing to watch and listen to
-          new things!
-        </p>
-      <h1 class="header">
-        Why I Made This Website
-      </h1>
-        <p>
-          This website is one of my first real solo web development
-          projects that I'm developing, and I hope to use it as 
-          a personal website. 
-        </p>
+      <img
+        style={styles.profilePicture}
+        src={require("./images/personal_picture.jpg")}
+        alt="Headshot"
+      />
+      <h1 class="header">Greetings!</h1>
+      <p>
+        Hi, I'm Bruce. I'm a senior student at Louisiana State University in
+        Baton Rouge, currently studying computer science with a concentration in
+        software engineering and a minor in Chinese. I'm excited and passionate
+        about developing full-stack software systems and love contributing to
+        all kinds of innovative and impactful software engineering solutions.
+      </p>
+      <h1 class="header">Who I Am</h1>
+      <p>
+        Aside from working on completing my bachelor's degree in computer
+        science at LSU, I've also been actively working on a MERN stack web
+        application to support an LSU professor's kinesiology research for a few
+        months, and I'm also involved in research creating an Android app in
+        Kotlin to monitor voice calls for mobile security.
+      </p>
+      <h1 class="header">My Qualifications and Professional Interests</h1>
+      <p>
+        I'm mostly focused on front-end development but have experiences working
+        with multiple database architectures and machine learning models. I most
+        recently completed a software engineering internship at Tesla in
+        Fremont, CA, where I contributed to mechanical engineering auxiliary
+        applications with advanced React.js and Redux.js frontend skills and
+        Django/PostgreSQL database architecture. I've also completed internships
+        at Sparq, working to increase software consulting efficiency on a
+        scheduling app built in Spring Boot, MySQL, and React.js, as well as DXC
+        Technology, a software consulting company in New Orleans, LA, working
+        with AWS and MySQL databases to perform data ETL services, and Runatek,
+        a medtech startup in Dallas, Texas, working with React Native, Arduino
+        code, and materials to create marketable and beneficial medical devices.
+        I'm primarily interested in fullstack or frontend roles, but am quite
+        interested in exploring quantitative development, machine learning, and
+        any innovative software engineering solutions. I really enjoy working
+        with UI/UX development and learning more about web API and framework
+        development. Please see "Contact" info if interested in working with me,
+        as I'm always willing to work with new people and learn more about
+        software, web development, and other subjects!
+      </p>
+      <h1 class="header">Some Fun Facts</h1>
+      <p>
+        I really enjoy watching anime; my favorite anime right now are probably
+        Bleach, Black Clover, and Bungo Stray Dogs. I'm always willing to watch
+        and listen to new things! I'm also a big foodie and enjoy posting on my
+        food Instagram; I have some project ideas I've been working on related
+        to bringing my software skills to the food industry.
+      </p>
     </div>
   );
 };
-  
+
 export default About;
 
 let styles = {
   profilePicture: {
     borderRadius: 15,
     width: 500,
-    height: 'auto',
-    borderStyle: 'solid',
+    height: "auto",
+    borderStyle: "solid",
     borderWidth: 1,
-    borderColor: '#DAC4F7',
+    borderColor: "#DAC4F7",
   },
-}
+};
--- src/pages/qualifications.js
+++ src/pages/qualifications.js
@@ -1,13 +1,305 @@
-import React from 'react';
-import '../css/qualifications.css';
-  
+import React from "react";
+import "../css/qualifications.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@mui/icons-material/School";
+import WorkIcon from "@mui/icons-material/Work";
+import LocationOn from "@mui/icons-material/LocationOn";
+import LightbulbIcon from "@mui/icons-material/Lightbulb";
+
+import styled from "styled-components";
+import Tooltip from "@mui/material/Tooltip";
+
+const QualificationsWrapper = styled.div`
+  .sub-list {
+    margin-left: 5%;
+  }
+`;
+
 const Qualifications = () => {
   return (
-    <div>
-      <embed class="resume" type="image/jpg" src={require('./images/CV1.jpg')} width="100%" height="100%"></embed>
-      <embed class="resume" type="image/jpg" src={require('./images/CV2.jpg')} width="100%" height="100%"></embed>    
-    </div>
+    <QualificationsWrapper className="experience">
+      <h1 style={{ textAlign: "center" }}>Professional Experience</h1>
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="August 2020 - May 2024"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Louisiana State University, Baton Rouge, LA, USA
+          </h3>
+          <p>B.S. Computer Science (Software Engineering), Minor in Chinese</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="May 2023 - August 2023"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Software Engineering Intern -{" "}
+            <a href="https://www.tesla.com/" target="_blank">
+              Tesla
+            </a>
+          </h3>
+          {/* <div className="vertical-timeline-element-title">
+            <Tooltip title={"Hello"} placement="bottom-start">
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  margin: 0,
+                  backgroundColor: "rgba(232,231,244,1)",
+                  width: "20%",
+                  justifyContent: "center",
+                  borderRadius: 5,
+                  marginLeft: 5,
+                }}
+              >
+                <LightbulbIcon style={{ color: "#A0816C", marginLeft: 5 }} />
+                <div>Skills</div>
+              </div>
+            </Tooltip>
+          </div> */}
+          <div
+            className="vertical-timeline-element-title"
+            style={{ display: "flex", alignItems: "center", margin: 0 }}
+          >
+            <LocationOn style={{ color: "red", marginLeft: 5 }} />
+            <h3 style={{ marginLeft: -8 }}>Fremont, CA</h3>
+          </div>
+          <ul className="sub-list">
+            <li>
+              Enhanced React.js tools built with Ant Design and D3.js used
+              extensively by vehicle quality engineers.
+            </li>
+            <li>
+              Improved application performance up to 90% with Redux.js to access
+              extensive PostgreSQL enterprise data.
+            </li>
+            <li>
+              Implemented Python ElementTree XML parsing for creation of Pandas
+              dataframes uploaded to RESTful Django API endpoints, decreasing
+              vehicle design file download times by up to 50% for graphical
+              model construction.
+            </li>
+          </ul>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="January 2023 - May 2023"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Mobile App Development Intern -{" "}
+            <a href="https://furtrieve.com/" target="_blank">
+              Furtrieve
+            </a>
+          </h3>
+          <div
+            className="vertical-timeline-element-title"
+            style={{ display: "flex", alignItems: "center", margin: 0 }}
+          >
+            <LocationOn style={{ color: "red", marginLeft: 5 }} />
+            <h3 style={{ marginLeft: -8 }}>Fishers, IN (Remote)</h3>
+          </div>
+          <ul className="sub-list">
+            <li>
+              Developed React Native features for a social networking mobile
+              application in Xcode.
+            </li>
+            <li>
+              Augmented React.js security administration pages to monitor
+              communications by fetching MongoDB user data.
+            </li>
+            <li>
+              Implemented new community-based features through social posting
+              and notification mechanisms.
+            </li>
+          </ul>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="June 2022 - January 2023"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Web/Mobile App Developer Intern -{" "}
+            <a href="https://sciquel.org/" target="_blank">
+              SciQuel
+            </a>
+          </h3>
+          <div
+            className="vertical-timeline-element-title"
+            style={{ display: "flex", alignItems: "center", margin: 0 }}
+          >
+            <LocationOn style={{ color: "red", marginLeft: 5 }} />
+            <h3 style={{ marginLeft: -8 }}>Boston, MA (Remote)</h3>
+          </div>
+          <ul className="sub-list">
+            <li>
+              Designed the home feed page for a React Native science media
+              mobile application utilizing Node.js API calls and collaboration
+              with MongoDB developers to link to internal articles.
+            </li>
+            <li>
+              Constructed 3+ mobile application and website pages with HTML,
+              CSS, and React Native views.
+            </li>
+            <li>
+              Optimized React Native state/prop usage for navigable,
+              user-friendly pages and accessible content.
+            </li>
+          </ul>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="August 2022 - December 2022"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Software Development Junior Associate -{" "}
+            <a href="https://www.teamsparq.com/" target="_blank">
+              Sparq
+            </a>
+          </h3>
+          <div
+            className="vertical-timeline-element-title"
+            style={{ display: "flex", alignItems: "center", margin: 0 }}
+          >
+            <LocationOn style={{ color: "red", marginLeft: 5 }} />
+            <h3 style={{ marginLeft: -8 }}>Baton Rouge, LA (Hybrid)</h3>
+          </div>
+          <ul className="sub-list">
+            <li>
+              Designed efficient, scalable Java web apps/API endpoints to
+              increase client sourcing efficiency by up to 25%.
+            </li>
+            <li>
+              Built functional backend systems using Spring Boot and mastery of
+              the Agile software development lifecycle.
+            </li>
+            <li>
+              Integrated JUnit testing and Spring Data JPA to develop responsive
+              MySQL databases and React.js connections.
+            </li>
+          </ul>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="June 2022 - August 2022"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Technical Intern -{" "}
+            <a href="https://dxc.com/" target="_blank">
+              DXC Technology
+            </a>
+          </h3>
+          <div
+            className="vertical-timeline-element-title"
+            style={{ display: "flex", alignItems: "center", margin: 0 }}
+          >
+            <LocationOn style={{ color: "red", marginLeft: 5 }} />
+            <h3 style={{ marginLeft: -8 }}>New Orleans, LA (Hybrid)</h3>
+          </div>
+          <ul className="sub-list">
+            <li>
+              Designed an ETL pipeline to consolidate data into an AWS
+              relational database for machine learning queries.
+            </li>
+            <li>
+              Automated data transformation with extensive Python scripts in AWS
+              Lambda to increase weekly hiring manager efficiency in locating
+              developers for client projects by up to 40%.
+            </li>
+            <li>
+              Collaborated with frontend engineers to create Excel uploading
+              mechanisms with React and Ionic components.
+            </li>
+          </ul>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="May 2022 - August 2022"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Software Engineering Intern -{" "}
+            <a href="https://www.runatek.com/" target="_blank">
+              Runatek
+            </a>
+          </h3>
+          <div
+            className="vertical-timeline-element-title"
+            style={{ display: "flex", alignItems: "center", margin: 0 }}
+          >
+            <LocationOn style={{ color: "red", marginLeft: 5 }} />
+            <h3 style={{ marginLeft: -8 }}>Dallas, TX (Remote)</h3>
+          </div>
+          <ul className="sub-list">
+            <li>
+              Built C++ Arduino code used in circuits communicating between
+              React Native frontend GUI and medical devices.
+            </li>
+            <li>
+              Enhanced web application GUI by reconstructing Python Tkinter
+              interfaces in React Native.
+            </li>
+            <li>
+              Led startup medical software projects and collaborated with
+              product/material engineers to develop prototypes.
+            </li>
+          </ul>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="March 2021 - August 2021"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Frontend Developer/Instructor -{" "}
+            <a href="https://www.brbytes.org/" target="_blank">
+              BRBytes @ LSU
+            </a>
+          </h3>
+          <div
+            className="vertical-timeline-element-title"
+            style={{ display: "flex", alignItems: "center", margin: 0 }}
+          >
+            <LocationOn style={{ color: "red", marginLeft: 5 }} />
+            <h3 style={{ marginLeft: -8 }}>Baton Rouge, LA</h3>
+          </div>
+          <ul className="sub-list">
+            <li>
+              Built web pages on an educational program website with the Dhall
+              configuration language and Alpine.js.
+            </li>
+            <li>
+              Updated UI information with HTML/CSS knowledge and a
+              detail-oriented focus.
+            </li>
+            <li>
+              Instructed Louisiana teachers for certifications in Cybersecurity,
+              Computer Science, and other STEM courses.
+            </li>
+          </ul>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </QualificationsWrapper>
   );
 };
-  
+
 export default Qualifications;

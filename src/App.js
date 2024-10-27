bd78de52a19e2590b166352ba4f263bcf395d781
Bruce Quach <quachbruce@gmail.com>
2024-10-27 00:56:05 -0500

    Add default route for homepage

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/website" element={<Home />} />
         <Route path="/qualifications" element={<Qualifications />} />
         <Route path="/links" element={<Links />} />
         <Route path="/contact" element={<Contact />} />
